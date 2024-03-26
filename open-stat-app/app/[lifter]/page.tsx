018fb3ae4538345e310d9bc0a217c3972614a9ee
adam <adam@LAPTOP-D52M9NN4>
2024-03-26 23:18:47 +1000

    basic ui added and routing added

---
+++ open-stat-app/app/[lifter]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lifter = () => {
+  return (
+    <div>Lifter</div>
+  )
+}
+
+export default Lifter
---
+++ open-stat-app/app/home/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react'
+import SearchAppBar from '../../components/Appbar'
+
+const Home = () => {
+  return (
+    <>
+        <SearchAppBar />
+    </>
+  )
+}
+
+export default Home
